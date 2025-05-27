4be4269c60934a42e39c299a0a0aa0525e1e5d36
Gabriel do Rego Farias <gabriel.rfarias@a.ucb.br>
2025-05-27 18:22:39 -0300

    fix: sytax error

--- src/main/java/com/webook/app/config/SecurityConfig.java
+++ src/main/java/com/webook/app/config/SecurityConfig.java
@@ -1,3 +1,11 @@
+package com.webook.app.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+
 @Configuration
 public class SecurityConfig {
 
