f7024eddcf6fb49cbdf11be73443353428f73c16
Gabriel do Rego Farias <gabriel.rfarias@a.ucb.br>
2025-06-11 14:12:37 -0300

    update: security for deploy on github pages

--- src/main/java/com/webook/app/config/SecurityConfig.java
+++ src/main/java/com/webook/app/config/SecurityConfig.java
@@ -56,7 +56,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("http://localhost:5173"));
+        config.setAllowedOrigins(List.of("https://gabriel-farias-06.github.io"));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("Authorization", "Content-Type", "Accept"));
         config.setAllowCredentials(true);
